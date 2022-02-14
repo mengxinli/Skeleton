a7d44f4654ce7222d2c4f24c613a0211643f64b0
Ben Davies <P2573649@my365.dmu.ac.uk>
2022-02-14 03:34:25 +0000

    Added testing for staff class

---
+++ Testing1/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStaff
+
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff staffMember = new clsStaff();
+            Assert.IsNotNull(staffMember);
+
+        }
+    }
+}
